ca3d31ae796b29d594594c3927804586c4ed632a
tuannghia1710 <cinamontoastken1710.com>
2024-03-13 09:31:49 +0700

    baitap

---
+++ index.js
@@ -0,0 +1,24 @@
+function plus(){
+    let c = document.getElementById("a").value;
+    let d = document.getElementById("b").value;
+    let res = parseInt(c) + parseInt(d);
+    document.getElementById("result").innerHTML = res;
+}
+function diff(){
+    let c = document.getElementById("a").value;
+    let d = document.getElementById("b").value;
+    let res = parseInt(c) - parseInt(d);
+    document.getElementById("result").innerHTML = res;
+}
+function multiply(){
+    let c = document.getElementById("a").value;
+    let d = document.getElementById("b").value;
+    let res = parseInt(c) * parseInt(d);
+    document.getElementById("result").innerHTML = res;
+}
+function divide(){
+    let c = document.getElementById("a").value;
+    let d = document.getElementById("b").value;
+    let res = parseInt(c) / parseInt(d);
+    document.getElementById("result").innerHTML = res;
+}
